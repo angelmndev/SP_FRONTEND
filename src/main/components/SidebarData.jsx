880ce35ea7e4980db5d08213333bd840462a9be4
Miguel Angel <angelmn.dev@gmail.com>
2021-02-13 16:15:04 -0500

    Mantenimiento preventivo V.0001 frontend

---
+++ src/main/components/SidebarData.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+
+export const SidebarData = [
+    {
+        title: 'Rol',
+        path: '/rol',
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text'
+    }
+];
---
+++ src/rol/container/RolContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const RolContainer = () => {
+    return(
+        <h1>Rol</h1>
+    )
+}
+
+export default RolContainer;
